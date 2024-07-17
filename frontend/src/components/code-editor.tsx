38d8481fd1690014fc5528b0c1f0f2c38d4ce71a
chinathaip <cartoonabe@gmail.com>
2024-07-17 18:32:59 +0700

    fix docker build & clerk not loaded with monaco editor

--- frontend/src/components/code-editor.tsx
+++ frontend/src/components/code-editor.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { Box, Title } from "@mantine/core";
+import { useClerk } from "@clerk/nextjs";
 
 interface CodeEditorProps {
   value: string;
@@ -8,20 +9,23 @@ interface CodeEditorProps {
 }
 
 export default function CodeEditor({ value, onChange }: CodeEditorProps) {
+  const clerk = useClerk();
   return (
     <Box>
       <Title>Code Editor</Title>
-      <Editor
-        className="border border-gray-600 rounded p-1 "
-        height="80vh"
-        width="50vw"
-        defaultLanguage="html"
-        language="html, css"
-        defaultValue="<!-- Write your code here -->"
-        theme="vs-dark"
-        value={value}
-        onChange={(newValue) => onChange(newValue || "")}
-      />
+      {clerk.loaded && (
+        <Editor
+          className="border border-gray-600 rounded p-1 "
+          height="80vh"
+          width="50vw"
+          defaultLanguage="html"
+          language="html, css"
+          defaultValue="<!-- Write your code here -->"
+          theme="vs-dark"
+          value={value}
+          onChange={(newValue) => onChange(newValue || "")}
+        />
+      )}
     </Box>
   );
 }
